be7207a02c5bb765f8fb6f7437a3dd5b0e0eaa82
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-06-30 21:35:39 +0300

    Updates

---
+++ precache-manifest.fc669318db1ddb2da15940adfdd80db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db585245817f3eff518ea4020dd74a05",
+    "url": "/Simple_list_of_posts/index.html"
+  },
+  {
+    "revision": "e6c3a859af6e41937fd4",
+    "url": "/Simple_list_of_posts/static/css/main.accb845d.chunk.css"
+  },
+  {
+    "revision": "3fb94f96f807b8950e70",
+    "url": "/Simple_list_of_posts/static/js/2.85e13a6b.chunk.js"
+  },
+  {
+    "revision": "e6c3a859af6e41937fd4",
+    "url": "/Simple_list_of_posts/static/js/main.59488b7b.chunk.js"
+  },
+  {
+    "revision": "4f8da9b8a9bbf2088037",
+    "url": "/Simple_list_of_posts/static/js/runtime~main.5c2ede98.js"
+  },
+  {
+    "revision": "dd92fe156b94f26583150a3e2a7a8d22",
+    "url": "/Simple_list_of_posts/static/media/space.dd92fe15.jpg"
+  }
+]);
